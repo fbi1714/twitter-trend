b46c4f201f499a742a519f897e9b65c453d5dcba
Airwoman <airwomanzhy@hotmail.com>
2016-06-21 23:01:19 +1000

    data displayed. WIP

---
+++ db/migrate/20160621062907_trends.rb
@@ -0,0 +1,11 @@
+class Trends < ActiveRecord::Migration
+  def change
+    create_table :trends do |t|
+      t.string :name
+      t.text :location
+      t.integer :tweet_volume
+
+      t.timestamps null: false
+    end
+  end
+end
