4743f3495e2f4eb159e35ab26f320ed2ccb2cfb8
Airwoman <airwomanzhy@hotmail.com>
2016-06-23 00:43:58 +1000

    show bubble in homepage

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -2,13 +2,13 @@ class TrendsController < ApplicationController
   def index
     last_trend = Trend.last
     location = params[:location]
-    time_gap = params[:time_gap] || -24
+    time_gap = params[:time_gap] || -12
 
     if created_15_mintues_ago(last_trend)
       get_trends_from_twitter
     end
 
-    trends = Trend.where.not(value: nil).where(created_at: ((Time.now + (time_gap.to_i - 3).hours)...(Time.now + time_gap.to_i.hours )))
+    trends = Trend.where.not(value: nil).where(created_at: ((Time.now + (time_gap.to_i - 1).hours)...(Time.now + time_gap.to_i.hours )))
 
     if location && location != 'All'
       render :json => trends.where(location: location).to_json
