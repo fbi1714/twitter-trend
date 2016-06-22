6e96c4b5a83416c646437eb6b57d8df590dbb668
fbi1714 <fcamatti@ucs.br>
2016-06-22 14:34:05 +1000

    Fixing errors from the new routes file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,11 +17,11 @@ class HomeController < ApplicationController
 
   def json
     # Recent tweets
-    r1 = $twitter.search("anything", :count => 10)
-    r2 = $twitter.search("anything", :count => 10, :geocode => "-22.912214,-43.230182,100km", :lang => "pt", :result_type => "recent")
-
-    @all_data = r2.attrs
-    render :json => @all_data
+    # r1 = $twitter.search("anything", :count => 10)
+    # r2 = $twitter.search("anything", :count => 10, :geocode => "-22.912214,-43.230182,100km", :lang => "pt", :result_type => "recent")
+    #
+    # @all_data = r2.attrs
+    # render :json => @all_data
   end
 
 end
