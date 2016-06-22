2134c8ef07401681ff3f4424a4d877a5aacaa587
cedricamoyal <cedric.amoyal@gmail.com>
2016-06-22 18:06:32 +1000

    d33 working

--- app/models/trend.rb
+++ app/models/trend.rb
@@ -1,3 +1,9 @@
 class Trend < ActiveRecord::Base
-  
+  def self.get_tweets_around_time( time = Time.zone.now )
+    # Filter here based around a given time
+    # If the time was 1 o'clock today
+      # Get the trends with a created_at between 12:45 and 1:15
+    # Trend.where(:created_at => (Time.zone.now.beginning_of_day..Time.zone.now.end_of_day))
+    Trend.where(:created_at => ((time - 15.minutes)..(time + 15.minutes)))
+  end
 end
